d300010a224e856c6476cf5dcd68be73ea31fe6f
David Rideout <ufahamu@pm.me>
2022-01-19 12:29:38 -0800

    compute terms in inner product on layer 1

--- nn.cu
+++ nn.cu
@@ -3,23 +3,34 @@
 #include "nn.h"
 
 #define VERB 1
+#define NTHREADS (NBITS+1>NNEURONS ? NBITS+1 : NNEURONS) // max num inputs per neuron
+
+__global__ void learn(float *truth, float *in, float *a1, float *a2) {
+  __shared__ int temp[NNEURONS][NTHREADS];
+  int i = threadIdx.x; // index of dot product
+  int j = threadIdx.y; // index of neuron
+
+  /* Forward Propagate */
+  /* Layer 1 */
+
+  /* Populate local memory */
+  // if (i<NBITS+1) temp[j,i] = a1[AI(j,i)];
+
+  /* Compute inner product */
+  if (i<NBITS+1) temp[j][i] = a1[AI(j,i)] * in[i];
+  __syncthreads(); // sync threads between staging data and doing computation?
+  
+  
 
 
-__global__ void learn(float *truth, float *a1, float *a2) {
-
-  //__shared__ int temp[...];
-  //int i = threadIdx.x;
-
-  *truth += .5;
-
-  //__syncthreads(); // sync threads between staging data and doing computation?
-
-  //c[i] = a[i] + b[i];
 }
 
 
 int main(void) {
   int i; //, j;
+  const int nnum = (1<<NBITS)+1;
+  const int sf = sizeof(float);
+  //  const int nthreads = NBITS+1>NNEURONS ? NBITS+1 : NNEURONS;
 
   /* MPI */
   //int nproc=1, myproc=0;
@@ -35,25 +46,28 @@ int main(void) {
   /* Compute ground truth */
   const float y[] = {0,0,1.,1.,-1.,1.,-1.,1.,-1.,-1.,-1.,1.,-1.,1.,-1.,-1.};
   unsigned char num;
-  float in[16][5];
-  for (num=2; num<16; ++num) {
+  float in[nnum][NBITS+1];
+  for (num=2; num<nnum; ++num) {
     in[num][0] = 1.;
-    for (i=1; i<5; ++i) in[num][i] = num & 1<<i ? 1. : 0.;
+    for (i=1; i<NBITS+1; ++i) in[num][i] = num & 1<<i ? 1. : 0.;
   }
-  if (VERB) for (num=2; num<16; ++num) {
+  if (VERB) for (num=2; num<nnum; ++num) {
     printf("%2d %3.0f : ", num, y[num]);
     printf("%2.f %2.f %2.f %2.f\n", in[num][0], in[num][1], in[num][2],
 	   in[num][3]);
   }
 
   /* Define neural network */
-  float *a1, *z1, *a2, z2;
-  a1 = (float *) malloc((NBITS+1)*NNEURONS*sizeof(float));
-  z1 = (float *) malloc(NNEURONS*sizeof(float));
-  a2 = (float *) malloc(NNEURONS*sizeof(float));
-  for (i=0; i<(NBITS+1)*NNEURONS; ++i) a1[i] = 2*drand48()-1;
-  for (i=0; i<NNEURONS; ++i) z1[i] = 2*drand48()-1;
-  for (i=0; i<NNEURONS; ++i) a2[i] = 2*drand48()-1;
+  //float *a1, *z1, *a2, z2;
+  float *a1, *a2;
+  a1 = (float *) malloc((NBITS+1)*NNEURONS*sf);
+  //z1 = (float *) malloc(NNEURONS*sf);
+  a2 = (float *) malloc(NNEURONS*sf);
+  a1[0] = 0.; // start biases at 0
+  for (i=1; i<(NBITS+1)*NNEURONS; ++i) a1[i] = 2*drand48()-1;
+  //for (i=0; i<NNEURONS; ++i) z1[i] = 2*drand48()-1;
+  a2[0] = 0.;
+  for (i=1; i<NNEURONS; ++i) a2[i] = 2*drand48()-1;
   
   /* Training loop */
   unsigned int sweep=0;
@@ -69,15 +83,22 @@ int main(void) {
     for (num=2; num<1<<NBITS; ++num) {
 
       /* Stage nn computation on GPU */
-      float *dtruth, *da1, *da2;
-      cudaMalloc(&dtruth, sizeof(float));
-      cudaMalloc(&da1, sizeof(float)*(NBITS+1)*NNEURONS);
-      cudaMalloc(&da2, sizeof(float)*NNEURONS);
-      cudaMemcpy(dtruth, &y[num], sizeof(float), cudaMemcpyHostToDevice);
-      //cudaMemcpy(db, b, size, cudaMemcpyHostToDevice);
+      float *dtruth, *din, *da1, *da2;
+      cudaMalloc(&dtruth, sf);
+      cudaMalloc(&din, sf*(NBITS+1));
+      cudaMalloc(&da1, sf*(NBITS+1)*NNEURONS);
+      cudaMalloc(&da2, sf*NNEURONS);
+
+      cudaMemcpy(dtruth, &y[num], sf, cudaMemcpyHostToDevice);
+      cudaMemcpy(din, in[num], (NBITS+1)*sf, cudaMemcpyHostToDevice);
+      cudaMemcpy(da1, a1, (NBITS+1)*NNEURONS*sf, cudaMemcpyHostToDevice);
+      cudaMemcpy(da2, a2, NNEURONS*sf, cudaMemcpyHostToDevice);
 
       /* Run the kernel */
-      learn<<<1,1>>>(dtruth, da1, da2);
+      dim3 threads(NTHREADS,NNEURONS);
+      learn<<<1,threads>>>(dtruth, din, da1, da2);
+
+
 
       /* See how it went */
       cudaError_t cerr = cudaGetLastError();
@@ -87,7 +108,7 @@ int main(void) {
 
       /* Copy results back to host */
       float junk;
-      cudaMemcpy(&junk, dtruth, sizeof(float), cudaMemcpyDeviceToHost);
+      cudaMemcpy(&junk, dtruth, sf, cudaMemcpyDeviceToHost);
   //cudaMemcpy(&d, dd, sizeof(int), cudaMemcpyDeviceToHost);
 
       printf("Hello World! %f\n", junk);
